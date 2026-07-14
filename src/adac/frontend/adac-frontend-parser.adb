1d27718e16eab5593e5260b0dcff4f6ea52adba7
Hodong <hodong@nimfsoft.com>
2026-07-15 03:26:36 +0900

    parser: restore truncated source and qualify statement aggregate

--- src/adac/frontend/adac-frontend-parser.adb
+++ src/adac/frontend/adac-frontend-parser.adb
@@ -16,19 +16,4 @@ package body Adac.Frontend.Parser is
   use Adac.Frontend.Tokens;
 
   type Parser is limited record
-    scanner : Adac.Frontend.Lexer.Scanner;
-    current : Token;
-    unit    : Adac.AST.Compilation_Unit;
-    failed  : Boolean := False;
-  end record;
-
-  procedure advance (self : in out Parser) is
-  begin
-    self.current := Adac.Frontend.Lexer.next_token (self.scanner);
-  end advance;
-
-  procedure report_expected (self     : in out Parser;
-                             expected : Token_Kind)
-  is
-    text : constant String :=
-      Ada
+    scanner : Adac.Frontend.L
